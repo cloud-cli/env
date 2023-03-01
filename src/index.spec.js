3e5d7e83f9f095ca9fd83b2eb5171b0ff1c51d26
Darlan Alves <me@darlanalv.es>
2023-03-01 15:19:44 +0000

    feat: first version

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,28 @@
-import env from "./index.js";
+import env from './index';
 
-describe("env", () => {
-  it("should show variables for an app", () => {
-    const output = env.show();
+beforeAll(() => {
+  process.env.IN_MEMORY_DB = true;
+});
+
+const app = { app: 'test' };
+const envVariable = { app: 'test', key: 'key', value: 'ok' };
+
+describe('env', () => {
+  it('should store variables for an app', async () => {
+    env.reload();
+    await expect(env.set({ app: '', key: '' })).rejects.toEqual(new Error('App not specified'));
+    await expect(env.set({ app: 'test', key: '' })).rejects.toEqual(new Error('Key not specified'));
+    await expect(env.get({ app: 'test', key: 'key' })).resolves.toEqual(undefined);
+
+    // upsert
+    await expect(env.set(envVariable)).resolves.toBeNull();
+    await expect(env.set(envVariable)).resolves.toBeNull();
+
+    await expect(env.get({ app: 'test', key: 'key' })).resolves.toEqual(envVariable);
+
+    await expect(env.show(app)).resolves.toEqual([envVariable]);
+
+    await expect(env.delete(envVariable)).resolves.toBeNull();
+    await expect(env.show(app)).resolves.toEqual([]);
   });
 });
---
+++ src/index.ts
@@ -0,0 +1,140 @@
+import sqlite from 'sqlite3';
+import { join } from 'path';
+
+const appNotSpecifiedError = new Error('App not specified');
+const keyNotSpecifiedError = new Error('Key not specified');
+let db: sqlite.Database;
+
+const columns = {
+  App: 'app',
+  Key: 'key',
+  Value: 'value',
+};
+
+export interface App {
+  app: string;
+}
+
+export interface KeyValue {
+  key: string;
+  value?: string;
+}
+
+export interface AppKeyValue extends App, KeyValue { }
+
+function close() {
+  try {
+    if (db) {
+      db.close();
+    }
+  } catch { };
+}
+
+function reload() {
+  close();
+
+  const inMemory = !!process.env.IN_MEMORY_DB;
+
+  if (inMemory) {
+    sqlite.verbose();
+  }
+
+  const databasePath = inMemory ? ':memory:' : join(process.cwd(), 'env.db');
+
+  db = new sqlite.Database(databasePath);
+  db.run(`CREATE TABLE IF NOT EXISTS env (
+      ${columns.App} TEXT,
+      ${columns.Key} TEXT,
+      ${columns.Value} TEXT,
+      UNIQUE (${columns.App}, ${columns.Key})
+  )`);
+}
+
+async function show(options: App) {
+  return new Promise((resolve, reject) => {
+    const { app } = options;
+
+    if (!app) {
+      return reject(appNotSpecifiedError);
+    }
+
+    db
+      .prepare(`
+        SELECT
+          ${columns.App},
+          ${columns.Key},
+          ${columns.Value}
+        FROM env WHERE
+        ${columns.App} = ?`
+      )
+      .all([app], (error, rows) => error ? reject(error) : resolve(rows));
+  });
+}
+
+async function set(options: AppKeyValue) {
+  const { app, key, value } = options;
+
+  if (!app) {
+    return Promise.reject(appNotSpecifiedError);
+  }
+
+  if (!key) {
+    return Promise.reject(keyNotSpecifiedError);
+  }
+
+  return query('REPLACE INTO env VALUES (?, ?, ?)', [app, key, value]);
+}
+
+async function remove(options: AppKeyValue) {
+  const { app, key } = options;
+
+  if (!app) {
+    return Promise.reject(appNotSpecifiedError);
+  }
+
+  if (!key) {
+    return Promise.reject(keyNotSpecifiedError);
+  }
+
+  return query(`DELETE FROM env WHERE ${columns.App} = ? AND ${columns.Key} = ?`, [app, key])
+}
+
+async function get(options: Omit<AppKeyValue, 'value'>) {
+  return new Promise((resolve, reject) => {
+    const { app, key } = options;
+
+    if (!app) {
+      return reject(appNotSpecifiedError);
+    }
+
+    if (!key) {
+      return reject(keyNotSpecifiedError);
+    }
+
+    db.serialize(() => {
+      db
+        .prepare(`
+          SELECT
+            ${columns.App},
+            ${columns.Key},
+            ${columns.Value}
+          FROM env WHERE
+            ${columns.App} = ? AND ${columns.Key} = ?`
+        )
+        .all([app, key], (error, rows) => (error ? reject(error) : resolve(rows[0])));
+    });
+  });
+}
+
+async function query(statement, inputs) {
+  return new Promise((resolve, reject) => {
+    db.serialize(() => {
+      db
+        .prepare(statement)
+        .run(inputs)
+        .finalize((error) => (error ? reject(error) : resolve(null)));
+    });
+  })
+}
+
+export default { reload, show, get, set, delete: remove };
