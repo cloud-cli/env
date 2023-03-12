4efd6475169004a180bdc1197170d7611799889f
Darlan Alves <me@darlanalv.es>
2023-03-12 16:27:27 +0530

    test: fix specs

--- src/index.spec.js
+++ src/index.spec.js
@@ -27,9 +27,9 @@ describe('env', () => {
     await expect(env.show(app)).resolves.toEqual([envVariable]);
     await expect(env.apps()).resolves.toEqual([{ app: 'test' }]);
 
-    await expect(env.delete({ app: '', key: '' })).rejects.toEqual(new Error('App not specified'));
-    await expect(env.delete({ app: 'test', key: '' })).rejects.toEqual(new Error('Key not specified'));
-    await expect(env.delete(envVariable)).resolves.toBeNull();
+    await expect(env.remove({ app: '', key: '' })).rejects.toEqual(new Error('App not specified'));
+    await expect(env.remove({ app: 'test', key: '' })).rejects.toEqual(new Error('Key not specified'));
+    await expect(env.remove(envVariable)).resolves.toBeNull();
     await expect(env.show(app)).resolves.toEqual([]);
   });
 });
